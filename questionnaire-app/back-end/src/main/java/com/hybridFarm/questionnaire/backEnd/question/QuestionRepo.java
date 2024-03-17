f87a6556ca442c7d8aed93c53185b103785fbf1b
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-03-17 19:30:34 +0530

    Added Question and Choice entities and repository interfaces.

---
+++ questionnaire-app/back-end/src/main/java/com/hybridFarm/questionnaire/backEnd/question/QuestionRepo.java
@@ -0,0 +1,6 @@
+package com.hybridFarm.questionnaire.backEnd.question;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepo extends JpaRepository<Question, Integer> {
+}
