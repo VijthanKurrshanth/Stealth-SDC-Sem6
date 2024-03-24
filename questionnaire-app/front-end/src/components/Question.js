079b15cb059981e40a79db29268ae94edea58382
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-03-24 15:52:04 +0530

    Add LoadingScreen component and handle loading state in Question component

--- questionnaire-app/front-end/src/components/Question.js
+++ questionnaire-app/front-end/src/components/Question.js
@@ -2,21 +2,33 @@ import { useState } from "react";
 import "../styles/Question.css";
 import { useEffect } from "react";
 import Choices from "./Choices";
+import LoadingScreen from "./LoadingScreen";
 
 const Question = () => {
   const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+  const [loading, setLoading] = useState(false);
   const [questionId, setQuestionId] = useState(1);
   const [questionText, setQuestionText] = useState("");
   const [choices, setChoices] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch(backendUrl + "/" + questionId)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+        return response;
+      })
       .then((response) => response.json())
       .then((data) => {
         setQuestionText(data.question);
         setChoices(data.choices);
+        setLoading(false);
       })
-      .catch((error) => console.error("Error:", error));
+      .catch((error) => {
+        console.error("Error:", error);
+        setError(error);
+      });
   }, [backendUrl, questionId]);
 
   function handleChoiceClick(choice) {
@@ -24,9 +36,11 @@ const Question = () => {
     setQuestionId(questionId + 1);
   }
 
+  if (loading) return <LoadingScreen />;
+
   return (
     <div>
-      <header className="blankSpace"></header>
+      <header className="BlankSpace"></header>
       <h1 className="Question">{questionText}</h1>
       <Choices handleClick={handleChoiceClick} choices={choices} />
     </div>
