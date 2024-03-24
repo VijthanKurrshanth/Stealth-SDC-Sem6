2c5d2d6be3a366466dfa5ad3c3588fabdd79c8df
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-03-24 15:01:34 +0530

    Refactor Choices component and update Question component

--- questionnaire-app/front-end/src/components/Question.js
+++ questionnaire-app/front-end/src/components/Question.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
 import "../styles/Question.css";
+import { useEffect } from "react";
+import Choices from "./Choices";
 
 const Question = () => {
+  const backendUrl = process.env.REACT_APP_BACKEND_BASE_URL;
+  const [questionId, setQuestionId] = useState(1);
+  const [questionText, setQuestionText] = useState("");
+  const [choices, setChoices] = useState([]);
+
+  useEffect(() => {
+    fetch(backendUrl + "/" + questionId)
+      .then((response) => response.json())
+      .then((data) => {
+        setQuestionText(data.question);
+        setChoices(data.choices);
+      })
+      .catch((error) => console.error("Error:", error));
+  }, [backendUrl, questionId]);
+
+  function handleChoiceClick(choice) {
+    console.log(choice);
+    setQuestionId(questionId + 1);
+  }
+
   return (
     <div>
       <header className="blankSpace"></header>
-      <h1 className="Question">
-        This will be the question. This will be the question. This will be the
-        question. This will be the question.
-      </h1>
+      <h1 className="Question">{questionText}</h1>
+      <Choices handleClick={handleChoiceClick} choices={choices} />
     </div>
   );
 };
