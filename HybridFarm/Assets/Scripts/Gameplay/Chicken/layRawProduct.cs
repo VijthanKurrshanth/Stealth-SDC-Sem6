6c76eba3e90b40fb61d19a250fc3b0a5755d6b97
SAIRISAN123 <sairisan123@gmail.com.com>
2024-03-30 17:16:24 +0530

    chicken can lay egg with given random interval

---
+++ HybridFarm/Assets/Scripts/Gameplay/Chicken/layRawProduct.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layRawProduct : MonoBehaviour
+{
+    public GameObject RawProductPrefab; // Reference to the prefab you want to spawn
+    public float spawnInterval = 5f; // Interval between each spawn
+
+    private float timer = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the timer
+        timer = spawnInterval;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Decrease the timer
+        timer -= Time.deltaTime;
+
+        // If the timer reaches zero or less, spawn the prefab and reset the timer
+        if (timer <= 0)
+        {
+            SpawnRawProduct();
+            timer = Random.Range(spawnInterval-1,spawnInterval+1);
+
+        }
+    }
+
+    void SpawnRawProduct()
+    {
+        // Instantiate the egg prefab at the current object's position and rotation
+        Instantiate(RawProductPrefab, transform.position, transform.rotation);
+    }
+}
