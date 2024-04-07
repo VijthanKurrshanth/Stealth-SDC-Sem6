bfe149948a9503c13504c5ab20dd6d1c4d43fa9f
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-04-07 09:32:02 +0530

    Add AuthenticationController and update QuestionController

--- questionnaire-app/back-end/src/main/java/com/hybridFarm/questionnaire/backEnd/QuestionController.java
+++ questionnaire-app/back-end/src/main/java/com/hybridFarm/questionnaire/backEnd/QuestionController.java
@@ -13,15 +13,20 @@ import java.util.Map;
 @RequestMapping(path = "hybridFarm/v1")
 public class QuestionController {
     private final QuestionService questionService;
+    private final AuthenticationController authenticationController;
 
     @Autowired
-    public QuestionController(QuestionService questionService) {
+    public QuestionController(QuestionService questionService, AuthenticationController authenticationController) {
         this.questionService = questionService;
+        this.authenticationController = authenticationController;
     }
 
     // Get Question REST API
     @GetMapping("{id}")
     public ResponseEntity<?> getQuestion(@PathVariable("id") Integer id) {
+        if (!authenticationController.getIsAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         QuestionDto questionDto = questionService.getQuestionDto(id);
         return ResponseEntity.ok(questionDto);
     }
@@ -29,6 +34,9 @@ public class QuestionController {
     // Get Answer Sheet REST API
     @GetMapping
     public ResponseEntity<?> getAnswerSheet() {
+        if (!authenticationController.getIsAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         List<Map<String, String>> response = questionService.getAnswerSheet();
         return ResponseEntity.ok(response);
     }
@@ -36,12 +44,20 @@ public class QuestionController {
     // Get Final Score REST API
     @GetMapping("score")
     public ResponseEntity<?> getScore() {
+        if (!authenticationController.getIsAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        else
+            authenticationController.setIsAuthenticated(false);
         return ResponseEntity.ok(questionService.getScore());
     }
 
     // Receive Answer REST API
     @PostMapping("{id}")
     public ResponseEntity<?> receiveAnswer(@PathVariable("id") Integer questionId, @RequestParam("Answer-Id") Integer answerId){
+        if (!authenticationController.getIsAuthenticated()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
         questionService.evaluate(questionId, answerId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
