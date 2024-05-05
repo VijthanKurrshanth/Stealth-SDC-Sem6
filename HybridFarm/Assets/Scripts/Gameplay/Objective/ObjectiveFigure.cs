b39f63fbdc15cba842cfdbf2b14d16ab2078d570
SAIRISAN123 <sairisan123@gmail.com.com>
2024-05-05 21:56:18 +0530

    Objectie GUI sucessfull!!!!!!1

--- HybridFarm/Assets/Scripts/Gameplay/Objective/ObjectiveFigure.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Objective/ObjectiveFigure.cs
@@ -7,59 +7,60 @@ public class ObjectiveFigure : MonoBehaviour
     Objective objective;
     public TextMeshProUGUI objectiveText; // Reference to the TextMeshProUGUI component to display the timer
     public int noOfItemCollected = 1;
-    public GameObject ItemPrefab1; 
-    public GameObject ItemPrefab2; 
-    public GameObject ItemPrefab3; 
-    public int[] itemsNumberpostionsInArray;
+ 
+    public GameObject[] ObjectiveSelectedprefabs;
     private int flag =0;
+
 
     void Start()
     {
 
         objective = FindObjectOfType<Objective>();
-        itemsNumberpostionsInArray = new int[3];
-        itemsNumberpostionsInArray[0]=1; //for initialize
-        itemsNumberpostionsInArray[1]=1;
-        itemsNumberpostionsInArray[2]=1;
+        ObjectiveSelectedprefabs = new GameObject[3];
+        ObjectiveSelectedprefabs[0]=null;
+        ObjectiveSelectedprefabs[1]=null;
+        ObjectiveSelectedprefabs[2]=null;
+        // ItemPrefab1= objective.respectiveItemSprites[1] ;
+        // ItemPrefab2= objective.respectiveItemSprites[2] ;
+        // ItemPrefab3= objective.respectiveItemSprites[3] ;
+        
 
-        ItemPrefab1= objective.respectiveItemSprites[1] ;
-        ItemPrefab2= objective.respectiveItemSprites[2] ;
-        ItemPrefab3= objective.respectiveItemSprites[3] ;
 
-        Debug.Log(ItemPrefab1);
+
+        
 
         for (int i = 0; i < objective.items.Length; i++)
         {
             
             if (objective.items[i]>0)
             {
-                itemsNumberpostionsInArray[flag] = i;
+                ObjectiveSelectedprefabs[flag]=objective.respectiveItemSprites[i];
                 flag++;
             }
         }
 
-
-
-        if (itemsNumberpostionsInArray.Length <= 3)
+            // Instantiate and position item 1
+            Vector3 spawnPosition1 = new Vector3(6.05006402f, -3.85f, 16f);
+            if (ObjectiveSelectedprefabs[0])
             {
-                // Instantiate and position item 1
-                Vector3 spawnPosition1 = new Vector3(6.05006402f, -3.85f, 16f);
-                GameObject objectiveitem1 = Instantiate(ItemPrefab1, spawnPosition1, Quaternion.identity);
+                GameObject objectiveitem1 = Instantiate(ObjectiveSelectedprefabs[0], spawnPosition1, Quaternion.identity);
+            }
                 
                  
-                // Instantiate and position item 2
-                Vector3 spawnPosition2 = new Vector3(6.0516402f, -3.85f, 16f) + new Vector3(1.1f * 1, 0, 0);
-                GameObject objectiveitem2 = Instantiate(ItemPrefab2, spawnPosition2, Quaternion.identity);
-                
-                
-
-                // Instantiate and position item 3
-                Vector3 spawnPosition3 = new Vector3(6.116402f, -3.85f, 16f) + new Vector3(1.1f * 2, 0, 0);
-                GameObject objectiveitem3 = Instantiate(ItemPrefab3, spawnPosition3, Quaternion.identity);
-                
-                
+            // Instantiate and position item 2
+            Vector3 spawnPosition2 = new Vector3(6.0516402f, -3.85f, 16f) + new Vector3(1.1f * 1, 0, 0);
+            if (ObjectiveSelectedprefabs[1])
+            {
+            GameObject objectiveitem2 = Instantiate(ObjectiveSelectedprefabs[1], spawnPosition2, Quaternion.identity);
             }
-
+                
+            // Instantiate and position item 3
+            Vector3 spawnPosition3 = new Vector3(6.116402f, -3.85f, 16f) + new Vector3(1.1f * 2, 0, 0);
+            if (ObjectiveSelectedprefabs[2])
+            {
+            GameObject objectiveitem3 = Instantiate(ObjectiveSelectedprefabs[2], spawnPosition3, Quaternion.identity);
+            }
+                
     }
 
     void Update()
