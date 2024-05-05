0f2e2a30d2e0ba96d05058be7c97ab63854ce967
SAIRISAN123 <sairisan123@gmail.com.com>
2024-05-05 12:23:53 +0530

    objective and objective GUI progress

--- HybridFarm/Assets/Scripts/Gameplay/Objective/Objective.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Objective/Objective.cs
@@ -36,7 +36,7 @@ public class Objective : MonoBehaviour
         items = new int[] { amount_of_money, No_of_chicken, No_of_pig, No_of_cow, No_of_egg, No_of_eggPoweder, No_of_cake, No_of_meat, No_of_meatSlice, No_of_sausage, No_of_milk, No_of_curd, No_of_cheese };
         itemsname = new string[] { "Money","chicken", "pig", "cow", "egg", "eggPoweder", "cake", "meat", "meatSlice", "sausage", "milk", "curd", "cheese" };
       
-        respectiveItemSprites = new string[] { "","","", "Collectables Sheet-01_6", "Collectables Sheet-01_7","Collectables Sheet-01_8","Collectables Sheet-01_3","Collectables Sheet-01_4","Collectables Sheet-01_5","Collectables Sheet-01_2","Collectables Sheet-01_1","Collectables Sheet-01_0"};
+        respectiveItemSprites = new string[] { "Coin","","", "Collectables Sheet-01_6", "Collectables Sheet-01_7","Collectables Sheet-01_8","Collectables Sheet-01_3","Collectables Sheet-01_4","Collectables Sheet-01_5","Collectables Sheet-01_2","Collectables Sheet-01_1","Collectables Sheet-01_0"};
         
 
     }
