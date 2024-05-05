5cfc524cbe54bdea7f0e1e01bb6c6765a2fbb3b9
SAIRISAN123 <sairisan123@gmail.com.com>
2024-05-05 23:17:23 +0530

    objectiveGUI text successfull!!!!!!!!!!!!1

--- HybridFarm/Assets/Scripts/Gameplay/Objective/ObjectiveTextDisplay.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Objective/ObjectiveTextDisplay.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
+using TMPro;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.Rendering;
-using TMPro;
-
 
 public class ObjectiveTextDisplay : MonoBehaviour
 {
@@ -11,37 +11,33 @@ public class ObjectiveTextDisplay : MonoBehaviour
     Objective objective;
     ObjectiveFigure objectiveFigure;
 
+    [SerializeField] int index=0;
+
     void Start()
     {
         objective = FindObjectOfType<Objective>();
-        objectiveFigure=FindObjectOfType<ObjectiveFigure>();
+        objectiveFigure = FindObjectOfType<ObjectiveFigure>();
         ObjectiveText = GetComponent<TextMeshProUGUI>(); // Get the TextMeshProUGUI component attached to the same GameObject
         if (ObjectiveText == null)
         {
-            Debug.LogError("Timer Sript requires a TextMeshProUGUI component attached to the same GameObject.");
+            Debug.LogError("Timer Script requires a TextMeshProUGUI component attached to the same GameObject.");
             return;
         }
 
-        //Debug.Log(objectiveFigure.IndexPostioninObjectiveItems[0]);
-        //ObjectiveText.text= "1" + "/" + objectiveFigure.IndexPostioninObjectiveItems[0].ToString();   // need to get from colletables
-        if (objectiveFigure.IndexPostioninObjectiveItems.Length > 0)
-        {
-            Debug.Log(objectiveFigure.IndexPostioninObjectiveItems[0]);
-            ObjectiveText.text = "1" + "/" + objectiveFigure.IndexPostioninObjectiveItems[0].ToString();
-        }
-        else
-        {
-            Debug.LogError("IndexPostioninObjectiveItems array is empty.");
-        }
-    
-    
-    
+        StartCoroutine(DisplayObjectiveText());
     }
+
+    private IEnumerator DisplayObjectiveText()
+    {
+        yield return new WaitForSeconds(0.01f); // Adjust the delay time as needed
+        if (objectiveFigure.IndexPostioninObjectiveItems[index]<13){
+        ObjectiveText.text = "1" + "/" + objectiveFigure.IndexPostioninObjectiveItems[index].ToString();
+        }
+    }
+
 
     void Update()
     {
         
     }
-
-    
 }
