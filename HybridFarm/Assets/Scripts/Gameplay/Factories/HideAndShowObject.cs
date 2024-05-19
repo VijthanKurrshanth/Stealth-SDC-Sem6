c6ce0b3dd980e2ce9aa0cd7f758d69728e0831fd
SAIRISAN123 <sairisan123@gmail.com.com>
2024-05-19 11:18:19 +0530

    hide and show script intiated 1

--- HybridFarm/Assets/Scripts/Gameplay/Factories/HideAndShowObject.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Factories/HideAndShowObject.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HideAndShowObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Array to hold all Renderer components of the GameObject and its children
+    private Renderer[] renderers;
+
     void Start()
     {
-        
+        // Get all Renderer components in the GameObject and its children
+        renderers = GetComponentsInChildren<Renderer>();
+
+        // Hide the object and its children when the game starts
+        SetRenderersEnabled(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if the "P" key is pressed
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            // Toggle the visibility of the object and its children
+            SetRenderersEnabled(!renderers[0].enabled);
+        }
+    }
+
+    // Method to enable or disable all Renderer components
+    private void SetRenderersEnabled(bool isEnabled)
+    {
+        foreach (Renderer renderer in renderers)
+        {
+            renderer.enabled = isEnabled;
+        }
     }
 }
