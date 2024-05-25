0736a907dea24f4a375f6a3bd57faecde93e1d5b
SAIRISAN123 <sairisan123@gmail.com.com>
2024-05-25 10:10:58 +0530

    facttory purachse button controller has been completed.

--- HybridFarm/Assets/Scripts/Gameplay/Factories/FactoryPriceHandler.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Factories/FactoryPriceHandler.cs
@@ -52,10 +52,7 @@ public class FactoryPriceHandler : MonoBehaviour
 
 
         
-        
-        
-   
-        
+    
         //factoryLevels = new string[] {"level1","level2", "level3","level4fuel","level4electric","level5fuel","level5electric"};
         eggPowderfactoryLevelsCost = new int[] {150,250,350,450,500,550,600};
         cakefactoryLevelsCost = new int[] {200,300,400,500,550,600,650};
