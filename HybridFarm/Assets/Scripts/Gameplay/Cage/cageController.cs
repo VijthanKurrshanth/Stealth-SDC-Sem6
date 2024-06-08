be71d0ebbb615368dfe5c87b3e81f406f9775697
SAIRISAN123 <sairisan123@gmail.com.com>
2024-06-08 21:25:50 +0530

    caged animal to warehouse

--- HybridFarm/Assets/Scripts/Gameplay/Cage/cageController.cs
+++ HybridFarm/Assets/Scripts/Gameplay/Cage/cageController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class cageController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] cagePrefabs; // Array to hold your 9 cage prefabs
+    public GameObject cagedAnimalPrefab; // Prefab for the caged animal
+    public float timeToReverse = 5.0f; // Time to start reversing the cage building process
+    private int cageStep = 0; // Current cage step
+    private GameObject currentCage; // Current displayed cage prefab
+    private float lastClickTime; // Time of the last click
+
     void Start()
     {
-        
+        lastClickTime = Time.time;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Check if enough time has passed to reverse the cage build
+        if (Time.time - lastClickTime > timeToReverse && cageStep > 0)
+        {
+            cageStep--;
+            UpdateCage();
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if (cageStep < cagePrefabs.Length)
+        {
+            cageStep++;
+            lastClickTime = Time.time; // Reset the timer
+            UpdateCage();
+        }
+    }
+
+    void UpdateCage()
+    {
+        if (currentCage != null)
+        {
+            Destroy(currentCage);
+        }
+
+        if (cageStep > 0 && cageStep < cagePrefabs.Length)
+        {
+            currentCage = Instantiate(cagePrefabs[cageStep - 1], transform.position, Quaternion.identity);
+            currentCage.transform.SetParent(this.transform);
+        }
+        else if (cageStep >= cagePrefabs.Length)
+        {
+            // Destroy the predator and spawn the caged animal
+            Destroy(gameObject); // Destroy the predator
+            Instantiate(cagedAnimalPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
