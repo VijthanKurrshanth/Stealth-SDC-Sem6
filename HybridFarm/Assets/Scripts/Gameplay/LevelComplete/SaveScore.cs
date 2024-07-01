efb67331bcae6747108660d134e9715e3aaa2c81
VijthanKurrshanth <kurrikurri0@outlook.com>
2024-07-01 19:32:41 +0530

    Fixed current level setting.

--- HybridFarm/Assets/Scripts/Gameplay/LevelComplete/SaveScore.cs
+++ HybridFarm/Assets/Scripts/Gameplay/LevelComplete/SaveScore.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SaveScore : MonoBehaviour
 {
@@ -30,7 +31,9 @@ public class SaveScore : MonoBehaviour
         Debug.Log("Sum of Level Scores: " + sum);
         playerScore = sum; // Update the player score with the sum of level scores
         PlayerPrefs.SetInt("PlayerScore", playerScore); // Save the updated score to PlayerPrefs
-        PlayerPrefs.SetInt("currentLevel", 2); // Set the current level to next level
+
+        int level = SceneManager.GetActiveScene().buildIndex - 4; // Get the level number based on the build index level 1 is 5, level 2 is 6, etc.
+        PlayerPrefs.SetInt("currentLevel", level); // Set the current level to next level
         PlayerPrefs.Save(); // Save the PlayerPrefs data
         Debug.Log("Player Score: " + PlayerPrefs.GetInt("PlayerScore")); // Log the player score
     }
