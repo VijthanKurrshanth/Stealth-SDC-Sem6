96ea8e8b002f1192a30cc3dfd3e7555df34642d0
SAIRISAN123 <sairisan123@gmail.com.com>
2024-08-07 11:42:22 +0530

    quit button

---
+++ HybridFarm/Assets/Scripts/Game Start/quitbutton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    public void Quit()
+    {
+        
+        #if UNITY_STANDALONE
+            Application.Quit();
+        #endif
+
+        
+        #if UNITY_EDITOR
+            
+            UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
